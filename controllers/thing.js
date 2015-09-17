7157a2ce6323a92ca058f9b55931dbbe5ae89191
Shaun McGlinchey <shaunmcglinchey@users.noreply.github.com>
2015-09-17 20:33:56 -0300

    Code refactor -Moved endpoints into a controller file

---
+++ controllers/thing.js
@@ -0,0 +1,75 @@
+// Load required packages
+var Thing = require('../models/thing');
+
+//TODO add validation on parameters
+
+// Create a new endpoint for POSTS
+exports.postThings = function(req, res) {
+    // Use our Things model
+    var thing = new Thing();
+
+    // instantiate our object with the values from our incoming POST request
+    thing.description = req.body.description;
+    thing.note = req.body.note;
+
+    // Save our thing
+    thing.save(function(err) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'New thing added to your todo list!', data: thing });
+    });
+};
+
+// Create a GET endpoint /api/things for fetching all to-do's
+exports.getThings = function(req, res) {
+    // Use our Things model
+    Thing.find(function(err, things) {
+        if (err)
+            res.send(err);
+
+        res.json(things);
+    });
+};
+
+// Create a GET endpoint /api/things/:thing_id for fetching a single thing
+exports.getThing = function(req, res) {
+    // Use our To-do model to fetch a single to-do item
+    Thing.findById(req.params.thing_id, function(err, thing) {
+        if (err)
+            res.send(err);
+
+        res.json(thing);
+    });
+};
+
+// Create a PUT endpoint /api/things/:thing_id for updating a single thing
+exports.putThing = function(req, res) {
+    // Using our Thing model fetch the thing that we want to update
+    Thing.findById(req.params.thing_id, function(err, thing) {
+        if (err)
+            res.send(err);
+
+        // Update the thing's description
+        thing.description = req.body.description;
+
+        // Save the thing
+        thing.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json(thing);
+        });
+    });
+};
+
+// Create a DELETE endpoint /api/things/:thing_id for deleting a single thing
+exports.deleteThing = function(req, res) {
+    // Using our Thing model fetch the thing that we want to delete
+    Thing.findByIdAndRemove(req.params.thing_id, function(err, thing) {
+        if (err)
+            res.send(err);
+
+        res.json({ message: 'Thing removed from your todo list!' });
+    })
+};
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var Thing = require('./models/thing');
+var thingController = require('./controllers/thing');
 
 // Connect to our mongo DB using mongoose
 mongoose.connect('mongodb://localhost:27017/thingly');
@@ -21,88 +21,16 @@ var port = process.env.PORT || 4000;
 // Create our Express router
 var router = express.Router();
 
-// Dummy route for testing
-router.get('/', function(req, res) {
-    res.json({ message: 'Your todo list is empty. Put your feet up!' });
-});
+// Create endpoint handlers for /things
+router.route('/things')
+    .post(thingController.postThings)
+    .get(thingController.getThings);
 
-// Define our things route
-var thingsRoute = router.route('/things');
-
-//TODO add validation on parameters
-
-// Create a new endpoint for POSTS
-thingsRoute.post(function(req, res) {
-    // Use our Things model
-    var thing = new Thing();
-
-    // instantiate our object with the values from our incoming POST request
-    thing.description = req.body.description;
-    thing.note = req.body.note;
-
-    // Save our thing
-    thing.save(function(err) {
-        if (err)
-           res.send(err);
-
-        res.json({ message: 'New thing added to your todo list!', data: thing });
-    });
-});
-
-// Create a GET endpoint /api/things for fetching all to-do's
-thingsRoute.get(function(req, res) {
-    // Use our Things model
-    Thing.find(function(err, things) {
-        if (err)
-           res.send(err);
-
-        res.json(things);
-    });
-});
-
-// Create a GET endpoint /api/things/:thing_id for fetching a single thing
-var thingRoute = router.route('/things/:thing_id');
-
-thingRoute.get(function(req, res) {
-   // Use our To-do model to fetch a single to-do item
-   Thing.findById(req.params.thing_id, function(err, thing) {
-     if (err)
-        res.send(err);
-
-     res.json(thing);
-   });
-});
-
-// Create a PUT endpoint /api/things/:thing_id for updating a single thing
-thingRoute.put(function(req, res) {
-    // Using our Thing model fetch the thing that we want to update
-    Thing.findById(req.params.thing_id, function(err, thing) {
-       if (err)
-         res.send(err);
-
-       // Update the thing's description
-       thing.description = req.body.description;
-
-       // Save the thing
-       thing.save(function(err) {
-           if (err)
-             res.send(err);
-
-           res.json(thing);
-       });
-    });
-});
-
-// Create a DELETE endpoint /api/things/:thing_id for deleting a single thing
-thingRoute.delete(function(req, res) {
-    // Using our Thing model fetch the thing that we want to delete
-    Thing.findByIdAndRemove(req.params.thing_id, function(err, thing) {
-        if (err)
-          res.send(err);
-
-        res.json({ message: 'Thing removed from your todo list!' });
-    })
-})
+// Create endpoint handlers for /things/:thing_id
+router.route('/things/:thing_id')
+    .get(thingController.getThing)
+    .put(thingController.putThing)
+    .delete(thingController.deleteThing);
 
 // Register all our routes under the /api path
 app.use('/api', router);
